c7684d3085d1fd7feb2b9af44ca1a6de8bb05a96
ciro9527 <ciro9527@gmail.com>
2020-12-04 11:01:12 +0800

    add bibox and bitmax

---
+++ bibox.py
@@ -0,0 +1,79 @@
+import json
+import base64
+import gzip
+import websockets
+from ccxtws.base import Exchange, ExchangeObserver
+from cryptobrick import logutils
+
+logger = logutils.get_logger('ccxtws')
+
+
+class bibox(Exchange):
+    def __init__(self):
+        self.ws_uri = 'wss://push.bibox.com'
+        self.observers = []
+        self.channels = set()
+        self.is_running = False
+
+    async def run(self):
+        if self.is_running:
+            return
+        self.is_running = True
+        while True:
+            try:
+                await self._run()
+            except Exception as e:
+                logger.exception(e)
+
+    async def _run(self):
+        async with websockets.connect(self.ws_uri) as websocket:
+            added_channels = set()
+            while True:
+                for channel in self.channels:
+                    if channel in added_channels:
+                        continue
+                    added_channels.add(channel)
+                    req = json.dumps({"event": "addChannel", "channel": f"bibox_sub_spot_{channel}_depth"})
+                    await websocket.send(req)
+                resp = await websocket.recv()
+                data = json.loads(resp)
+                if 'ping' in data:
+                    req = json.dumps({"pong": data['ping']})
+                    await websocket.send(req)
+                else:
+                    self.notify(data)
+
+    def subscribe(self, observer):
+        self.observers.append(observer)
+        self.channels.add(observer.channel)
+
+    def unsubscribe(self, observer):
+        self.observers.remove(observer)
+        self.channels = set([observer.channel for observer in self.observers])
+
+    def notify(self, data):
+        if len(data) > 1:
+            logger.warning("unknown data %s", data)
+            return
+        b64_data = data[0]['data']
+        gz_data = base64.b64decode(b64_data)
+        decoded_data = gzip.decompress(gz_data)
+        j_data = json.loads(decoded_data)
+        final_data = {'full': True, 'asks': [], 'bids': []}
+        final_data['asks'] = [[float(item['price']), float(item['volume'])] for item in j_data['asks']]
+        final_data['bids'] = [[float(item['price']), float(item['volume'])] for item in j_data['bids']]
+
+        for observer in self.observers:
+            if observer.channel not in data[0]['channel']:
+                continue
+            observer.update(final_data)
+
+
+class bibox_observer(ExchangeObserver):
+    def __init__(self, exchange, symbol, callback):
+        market = exchange.market(symbol)
+        self.channel = market['id']
+        self.callback = callback
+
+    def update(self, data):
+        self.callback(data)
---
+++ bitmax.py
@@ -0,0 +1,78 @@
+import json
+import websockets
+from ccxtws.base import Exchange, ExchangeObserver
+from cryptobrick import logutils
+
+logger = logutils.get_logger('ccxtws')
+
+
+class bitmax(Exchange):
+    def __init__(self):
+        self.ws_uri = 'wss://bitmax.io/0/api/pro/v1/stream'
+        self.observers = []
+        self.channels = set()
+        self.is_running = False
+
+    async def run(self):
+        if self.is_running:
+            return
+        self.is_running = True
+        while True:
+            try:
+                await self._run()
+            except Exception as e:
+                logger.exception(e)
+
+    async def _run(self):
+        async with websockets.connect(self.ws_uri) as websocket:
+            added_channels = set()
+            while True:
+                for channel in self.channels:
+                    if channel in added_channels:
+                        continue
+                    added_channels.add(channel)
+                    req = json.dumps({"op": "sub", "ch": f"depth:{channel}"})
+                    await websocket.send(req)
+                    req = json.dumps({"op": "req", "action": "depth-snapshot-top100", "args": {"symbol": channel}})
+                    await websocket.send(req)
+                resp = await websocket.recv()
+                data = json.loads(resp)
+                if data['m'] == 'ping':
+                    req = json.dumps({'op': 'pong'})
+                    await websocket.send(req)
+                elif data['m'] in ['depth-snapshot', 'depth']:
+                    self.notify(data)
+                else:
+                    logger.warning("unknown data %s", data)
+
+    def subscribe(self, observer):
+        self.observers.append(observer)
+        self.channels.add(observer.channel)
+
+    def unsubscribe(self, observer):
+        self.observers.remove(observer)
+        self.channels = set([observer.channel for observer in self.observers])
+
+    def notify(self, data):
+        final_data = {'asks': [], 'bids': []}
+        if data['m'] == 'depth-snapshot':
+            final_data['full'] = True
+        else:
+            final_data['full'] = False
+        final_data['asks'] = [[float(item[0]), float(item[1])] for item in data['data']['asks']]
+        final_data['bids'] = [[float(item[0]), float(item[1])] for item in data['data']['bids']]
+
+        for observer in self.observers:
+            if observer.channel != data['symbol']:
+                continue
+            observer.update(final_data)
+
+
+class bitmax_observer(ExchangeObserver):
+    def __init__(self, exchange, symbol, callback):
+        market = exchange.market(symbol)
+        self.channel = market['id']
+        self.callback = callback
+
+    def update(self, data):
+        self.callback(data)
