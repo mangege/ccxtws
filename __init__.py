78442ee8342b5494aa59e730ec9c23a5c0be1e33
ciro9527 <ciro9527@gmail.com>
2020-12-04 11:20:38 +0800

    fix import

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from ccxtws.poloniex import poloniex, poloniex_observer  # noqa: F401
 from ccxtws.biki import biki, biki_observer  # noqa: F401
 from ccxtws.huobipro import huobipro, huobipro_observer  # noqa: F401
 from ccxtws.gateio import gateio, gateio_observer  # noqa: F401
+from ccxtws.bibox import bibox, bibox_observer  # noqa: F401
+from ccxtws.bitmax import bitmax, bitmax_observer  # noqa: F401
 
 exchanges_ws = [
     'poloniex', 'poloniex_observer',
